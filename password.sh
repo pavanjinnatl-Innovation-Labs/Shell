1fd5e4331037331c5f6b82df20558ee18b49c71b
pavan <pavanjinnatl@gmail.com>
2026-06-22 13:18:18 +0530

    passwd

---
+++ password.sh
@@ -0,0 +1,8 @@
+echo "please enter your username::"
+read USER_NAME # here username is variable, what ever you enter in terminal in this variable
+echo "username is $USER_NAME"
+
+echo "please enter youe password::"
+read -s PASSWORD 
+
+echo "password is $PASSWORD"
